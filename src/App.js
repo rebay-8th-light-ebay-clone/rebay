d595a8477c84ba6865b404fa10530e96fa23576a
serpient <littlegangwolf@gmail.com>
2019-07-17 11:11:00 -0700

    Add Router setup
    To prove everything is setup for local development, quick and simple ItemsPage and ItemPage components were created. ItemsPage uses useEffect and axios to ensure our API call uses proxy correctly when the backend server is running locally. Now that it's proven to work, we can scrap and start again using TDD.
    
    - All Items Page has "/" and "/items" route
    - SIngle Item Page has the following route "/item/:id"
    - Add a localhost:4000 proxy which will route API calls only during development. We will still need a proper setup for the base endpoint for deployment.
    - Add Axios dependency to make API calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ItemsPage from 'views/ItemsPage';
+import ItemPage from 'views/ItemPage';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
         Rebay
       </header>
+      <Router>
+        <Route path="/" exact component={ItemsPage} />
+        <Route path="/items" exact component={ItemsPage} />
+        <Route path="/item/:id" exact component={ItemPage} />
+      </Router>
     </div>
   );
 }
