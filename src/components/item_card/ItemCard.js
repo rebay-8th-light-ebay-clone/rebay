09b7dde6b2938b5fb6ca007100511005c43aa6b5
serpient <littlegangwolf@gmail.com>
2019-07-19 09:36:10 -0700

    Add links from item card to item page
    - Add Header link, when clicked, to redirect back to "/"
    - Adjust styling for links
    - Add responsive styling to Item Page

--- src/components/item_card/ItemCard.js
+++ src/components/item_card/ItemCard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './ItemCard.scss';
 
 const ItemCard = ({ item }) => {
-    let { title, price, image, end_date } = item;
+    let { title, price, image, end_date, id } = item;
 
     return (
-        <section className='card item--card-container'>
-            <h1>{title}</h1>
-            <h2>${price}</h2>
-            <p>{countdown(new Date(), new Date(end_date))}</p>
-            <img src={image} alt={title} />
-        </section>
+        <a href={`/items/${id}`}>
+            <section className='card item--card-container'>
+                <h1>{title}</h1>
+                <h2>${price}</h2>
+                <p>{countdown(new Date(), new Date(end_date))}</p>
+                <img src={image} alt={title} />
+            </section>
+        </a>
     )
 }
 
