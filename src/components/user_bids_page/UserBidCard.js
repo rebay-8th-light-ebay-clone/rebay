17933ce5062dd23889ea20efb456d27069d62d74
serpient <littlegangwolf@gmail.com>
2019-08-07 08:53:36 -0700

    Inject bid limit for UserBidCard

--- src/components/user_bids_page/UserBidCard.js
+++ src/components/user_bids_page/UserBidCard.js
@@ -3,13 +3,15 @@ import ItemCard from 'components/item_card/ItemCard';
 import './UserBidCard.scss';
 import BidCard from '../bid_card/BidCard';
 
-const UserBidCard = ({ item, bids }) => {
-    const lastThreeBids = bids.length > 3 ? bids.slice(bids.length - 3) : bids;
+const UserBidCard = ({ item, bids, numOfBids = 3 }) => {
+    const mostRecentBids = (bidLimit) => {
+       return bids.length > bidLimit ? bids.slice(bids.length - bidLimit) : bids;
+    }
     return (
         <section className='user-bid--container'>
             <ItemCard item={item} showDescription={true} />
             <section className='user-bids--grid'>
-                {bids && lastThreeBids.map(bid => {
+                {bids && mostRecentBids(numOfBids).map(bid => {
                     return <BidCard bid_price={bid.bid_price} timestamp={bid.timestamp} key={bid.uuid} userName={"You"} winner={bid.winner} />
                 })}
             </section>
---
+++ src/components/user_bids_page/UserBidCard.test.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import UserBidCard from './UserBidCard';
+import { cleanup, render, waitForElement } from '@testing-library/react';
+import MockAPIHandler from 'utilities/APIHandler/mockApiHandler';
+
+describe('User Bids Card Test', () => {
+    afterEach(() => {
+        cleanup();
+    })
+
+    const item = {
+        "description": "test description",
+        "end_date": "2019-07-17T16:53:52Z",
+        "id": 1,
+        "image": "test-image-url",
+        "price": 10,
+        "title": "test title",
+        "current_highest_bid": 10
+    }
+
+    const bids = [
+        {
+            "bid_uuid": 1,
+            "user_uuid": 1,
+            "item_uuid": 1,
+            "bid_price": 15,
+            "timestamp": "2019-07-17T16:53:52Z",
+        },
+        {
+            "bid_uuid": 1,
+            "user_uuid": 1,
+            "item_uuid": 1,
+            "bid_price": 200,
+            "timestamp": "2019-07-17T16:53:52Z",
+        },
+        {
+            "bid_uuid": 1,
+            "user_uuid": 1,
+            "item_uuid": 1,
+            "bid_price": 300,
+            "timestamp": "2019-07-17T16:53:52Z",
+        },        {
+            "bid_uuid": 1,
+            "user_uuid": 1,
+            "item_uuid": 1,
+            "bid_price": 400,
+            "timestamp": "2019-07-17T16:53:52Z",
+        }
+    ]
+
+    test('renders an item with the correct data', async () => {
+        const component = render(<UserBidCard item={item} bids={bids} />);
+
+        await waitForElement(() =>
+            component.findAllByText('test title')
+        );
+        expect(component.queryByText(/0.15/i)).toBeNull();
+        component.getByText(/2.00/i);
+        component.getByText(/3.00/i);
+        component.getByText(/4.00/i);
+    })
+})
